9717dd8f1e99452738cb9027e6f4565f0d467697
MalRadar <MalRadar@outlook.com>
2025-01-23 14:52:14 +0800

    update response tips

--- frontend/src/components/GuidingResponseTips.tsx
+++ frontend/src/components/GuidingResponseTips.tsx
@@ -5,8 +5,10 @@ const GuidingResponseTips = () => (
     message="Please read the above app description and answer the following questions:"
     description={
       <p style={{ margin: '0 0 4px', fontSize: '14px', lineHeight: '1.5', color: '#555' }}>
-        What's your perception of the data access behavior? Indicate your <strong style={{ color: '#fa541c' }}>level of comfort</strong> based on the
-        app's described behavior for each scenario.
+        What's your perception of the data access behavior? Indicate your{' '}
+        <strong style={{ color: '#fa541c' }}>level of comfort</strong> based on the app's
+        functionality and the described purpose for each data access (cconsider whether the purpose
+        aligns with what you find acceptable).
       </p>
     }
     type="info"
